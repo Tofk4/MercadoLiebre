cd6e09bd03416d17b58380789e57a281dca8581b
Tomas <tomas2001ferr@gmail.com>
2021-07-29 20:49:22 -0300

    Subi el archivo

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express= require('express');
 const app = express();
 const path = require('path');
+const port = process.env.PORT || 3000
 const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath));
 app.get('/', function(reque,resp){resp.sendFile(path.join(__dirname, 'views/home.html'))});
 app.get('/login', function(reque,resp){resp.sendFile(path.join(__dirname, 'views/login.html'))});
 app.get('/registro', function(reque,resp){resp.sendFile(path.join(__dirname, 'views/registro.html'))});
-app.listen(3030, ()=> {
-    console.log('Pagina corriendo en http://localhost:3030')
+app.listen(port, function() {
+    console.log('Servido corriendo en corriendo en el puerto'+ port);
 })
