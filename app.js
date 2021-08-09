5334c8adbb9655d7cd8045a98aa703601e3f28dd
Tomas <tomas2001ferr@gmail.com>
2021-08-09 18:16:45 -0300

    Login y registro

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const port = process.env.PORT || 3000
 const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath));
-app.get('/home', function(reque,resp){resp.sendFile(path.join(__dirname, 'views/home.html'))});
+app.get('/', function(reque,resp){resp.sendFile(path.join(__dirname, 'views/home.html'))});
 app.get('/login', function(reque,resp){resp.sendFile(path.join(__dirname, 'views/login.html'))});
 app.get('/registro', function(reque,resp){resp.sendFile(path.join(__dirname, 'views/registro.html'))});
 app.listen(port, function() {
